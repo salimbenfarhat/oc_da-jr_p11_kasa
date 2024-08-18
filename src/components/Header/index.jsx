1fd859fccfc6736e95ec1ca119caef73b4fd3a72
Salim Benfarhat <hello@salim.link>
2024-08-18 09:56:54 +0200

    add underline in current menu element

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,24 @@
 // src/components/Header/index.jsx
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/img/LOGO_FULL.png';
 import '../Header/index.scss';
 
 function Header() {
+  const location = useLocation();
   return (
     <header className="header">
       <Link to="/">
         <img src={logo} className="header__logo" alt="Kasa Logo" />
       </Link>
       <nav className="header__nav">
-        <Link to="/" className="header__nav-link montserrat-500">Accueil</Link>
-        <Link to="/a-propos" className="header__nav-link montserrat-500">A Propos</Link>
+        <Link
+          to="/"
+          className={`header__nav-link montserrat-500 ${location.pathname === '/' ? 'active' : ''}`}
+        >Accueil</Link>
+        <Link
+          to="/a-propos"
+          className={`header__nav-link montserrat-500 ${location.pathname === '/a-propos' ? 'active' : ''}`}
+        >A Propos</Link>
       </nav>
     </header>
   );
